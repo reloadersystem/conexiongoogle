6e1bc6b5c21fbe4ee3c7595ac633fb0a8a2c9c49
Reloader <suarezfc82@gmail.com>
2020-04-24 09:16:14 -0500

    Initial Commit

---
+++ SignGoogle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SignGoogle
+//
+//  Created by Resembrink Correa on 4/24/20.
+//  Copyright © 2020 Reloader. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
